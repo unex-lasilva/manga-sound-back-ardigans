a1a2b97bf6f3bca8103a6f2307f32750d6cf59e3
DanielCaroba <109287556+DanielCaroba@users.noreply.github.com>
2025-04-25 19:21:42 -0300

    Create No.java (#5)
    Criando class No

---
+++ manga-sound-player/src/main/java/br/com/mangarosa/No.java
@@ -0,0 +1,28 @@
+package br.com.mangarosa;
+
+public class No {
+    private Object valor;
+    private No prox;
+
+    public No() {}
+
+    public No(Object valor) {
+        this.valor = valor;
+    }
+
+    public Object getValor() {
+        return valor;
+    }
+
+    public void setValor(Object valor) {
+        this.valor = valor;
+    }
+
+    public No getProx() {
+        return prox;
+    }
+
+    public void setProx(No prox) {
+        this.prox = prox;
+    }
+}
