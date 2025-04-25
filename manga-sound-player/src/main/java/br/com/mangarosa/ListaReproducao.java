366cfa656d94c1ef518ed7dc8cee4ae86ee8671d
Gustavo Prates Caetano <gustavoprates.c@hotmail.com>
2025-04-25 19:23:20 -0300

    Create ListaReproducao.java (#7)
    Criando a Class ListaReproducao, reproduzir a musica

---
+++ manga-sound-player/src/main/java/br/com/mangarosa/ListaReproducao.java
@@ -0,0 +1,56 @@
+package br.com.mangarosa;
+
+public class ListaReproducao {
+    private String titulo;
+    private ListaEncadeada lista;
+
+    public ListaReproducao(String titulo) {
+        this.titulo = titulo;
+        this.lista = new ListaEncadeada();
+    }
+
+    public void addMusica(Musica musica) {
+        lista.append(musica);
+    }
+
+    public boolean removerMusica(int posicao) {
+        return lista.remove(posicao);
+    }
+
+    public void inserirMusicaEm(int posicao, Musica musica) {
+        lista.insertAt(posicao, musica);
+    }
+
+    public boolean isVazia() {
+        return lista.isEmpty();
+    }
+
+    public int tamanho() {
+        return lista.size();
+    }
+
+    public void criarListaAPartirDe(ListaReproducao outra) {
+        this.lista.clear();
+        this.lista.addAll(outra.lista);
+    }
+
+    public int posicaoDa(Musica musica) {
+        return lista.indexOf(musica);
+    }
+
+    public boolean contemMusica(Musica musica) {
+        return lista.contains(musica);
+    }
+
+    public Musica obterMusica(int posicao) {
+        return (Musica) lista.get(posicao);
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
